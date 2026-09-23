398bce6fa90f1e9809c5a21e34a6cd9c890be52c
UmarQureshi-31 <qureshiumar206206@gmail.com>
2026-09-23 15:55:46 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import dns from "node:dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host} `
+    );
+  } catch (error) {
+    console.error("MONGODB connection FAILED", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
+
